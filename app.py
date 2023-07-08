eed07b4a21c3bb596b008d2526b9177188188ed2
rodriV <utndavid@gmail.com>
2023-07-07 21:52:41 -0300

    agrega comentario de funciones

--- app.py
+++ app.py
@@ -38,7 +38,31 @@ def mostrar_nuevo_tablero(palabra_elegida):
 
     print(lista_oculta)
 
+def pedir_letra():
+    '''
+    Esta función se encarga de pedir una letra al jugador.
+    También debe validar si la letra ingresada es valida(
+    pertenezca al abecedario y que esté ingresando solo UNA letra
+    )
+    '''
+    pass
+
+def chequear_letra():
+    pass
+
 palabra_elegida, letras_unicas = elegir_palabra(palabras)
 
-# while not juego_terminado:
-#     pass 
+while not juego_terminado:
+    # llamar a la funcion mostrar_nuevo_tablero
+
+    # mostrar letras incorrectas (con un print)
+
+    # mostrar vidas "intentos" 
+
+    # pedir al jugador que ingrese una letra llamando a una funcion pedir_letra
+
+    # llamar a una función que valide si ingresó un dato válido y si adivinó  una letra
+    intentos, terminado, aciertos = chequear_letra(letra,palabra,intentos,aciertos)
+    #MUY IMPORTANTE se debe modificar la variable juego_terminado
+    
+    juego_terminado = terminado
